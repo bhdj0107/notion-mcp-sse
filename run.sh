3c71096e0eb9068c573a6c471d005c5f726107cf
dongjun.lee <dongjun.lee@dongjunleeui-MacBookPro.local>
2025-04-21 00:06:59 +0900

    add health probe

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 npx -y supergateway \
     --stdio "npx -y @suekou/mcp-notion-server" \
     --port 3000 --baseUrl http://localhost:3000 \
-    --ssePath /sse --messagePath /message
+    --ssePath /sse --messagePath /message \
+    --healthEndpoint /health
